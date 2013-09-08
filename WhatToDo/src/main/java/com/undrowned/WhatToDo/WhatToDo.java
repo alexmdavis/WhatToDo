43b50aaf0cb5ae5f02c9429d0bf2e0eb472aad7f
Alex Davis <amdavis@gmail.com>
2013-09-07 20:59:11 -0400

    final 1.1

--- WhatToDo/src/main/java/com/undrowned/WhatToDo/WhatToDo.java
+++ WhatToDo/src/main/java/com/undrowned/WhatToDo/WhatToDo.java
@@ -1,18 +1,3 @@
-/*
- * Copyright (c) 2011 Google Inc.
- * 
- * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except
- * in compliance with the License. You may obtain a copy of the License at
- * 
- * http://www.apache.org/licenses/LICENSE-2.0
- * 
- * Unless required by applicable law or agreed to in writing, software distributed under the License
- * is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express
- * or implied. See the License for the specific language governing permissions and limitations under
- * the License.
-
-    MODIFIED for com.undrowned.WhatToDo 2013 Alex Davis
- */
 package com.undrowned.WhatToDo;
 
 import android.accounts.AccountManager;
@@ -31,7 +16,6 @@ import android.view.MenuItem;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
-import android.widget.Button;
 import android.widget.Spinner;
 import android.widget.TextView;
 
@@ -55,44 +39,27 @@ import java.util.Random;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
+
 /**
- * Sample activity for Google Tasks API v1. It demonstrates how to use authorization to list tasks
- * with the user's permission.
+ * Sole activity for WhatToDo app.
  *
- * @author Yaniv Inbar
+ * @author Alex Davis
  */
 public final class WhatToDo extends Activity {
 
-    /**
-     * Logging level for HTTP requests/responses.
-     *
-     * <p>
-     * To turn on, set to {@link Level#CONFIG} or {@link Level#ALL} and run this from command line:
-     * </p>
-     *
-     * <pre>
-     adb shell setprop log.tag.HttpTransport DEBUG
-     * </pre>
-     */
-    private static final Level LOGGING_LEVEL = Level.OFF;
-    private static final String PREF_ACCOUNT_NAME = "accountName";
-    static final String TAG = "WhatToDo";
-    static final int REQUEST_GOOGLE_PLAY_SERVICES = 0;
-    static final int REQUEST_AUTHORIZATION = 1;
-    static final int REQUEST_ACCOUNT_PICKER = 2;
-
+    // Google Tasks API access
     final HttpTransport transport = AndroidHttp.newCompatibleTransport();
     final JsonFactory jsonFactory = new GsonFactory();
     GoogleAccountCredential credential;
-
     com.google.api.services.tasks.Tasks service;
 
+    // UI and resources
     private TextView taskTextView;
     private Spinner tasklistSpinner;
     private Spinner moodSpinner;
-    private Button button_pick;
     private Resources res;
 
+    // Tasks and related data
     Task task;
     TaskList tasklist;
     List<String> tasklistnames = new ArrayList<String>();
@@ -108,8 +75,16 @@ public final class WhatToDo extends Activity {
             android.R.color.holo_red_light // angry
     };
 
-    int numAsyncTasks;
+    // Logging and accounts
+    private static final Level LOGGING_LEVEL = Level.OFF;
+    private static final String PREF_ACCOUNT_NAME = "accountName";
+    static final String TAG = "WhatToDo";
+    static final int REQUEST_GOOGLE_PLAY_SERVICES = 0;
+    static final int REQUEST_AUTHORIZATION = 1;
+    static final int REQUEST_ACCOUNT_PICKER = 2;
 
+    // Bookkeeping
+    int numAsyncTasks;
     Boolean init = Boolean.TRUE;
 
     @Override
@@ -166,26 +141,49 @@ public final class WhatToDo extends Activity {
         service =
                 new com.google.api.services.tasks.Tasks.Builder(transport, jsonFactory, credential)
                         .setApplicationName("WhatToDo/1.1")
-                        .setTasksRequestInitializer(new TasksRequestInitializer("AIzaSyCmE_OqAeCCws5bErfP2c4WeW5B5KtuET0"))
+                        .setTasksRequestInitializer(new TasksRequestInitializer(getString(R.string.google_api_key)))
                         .build();
 
         res = getResources();
         moods = res.getStringArray(R.array.spinner_moods);
     }
 
+    /**
+     * Changes end color for background gradient of given view.
+     *
+     * @param viewID
+     * @param moodColor new gradient end color
+     */
     private void setMoodGradient(int viewID, int moodColor) {
         ((GradientDrawable)findViewById(viewID).getBackground()).setColors(
                 new int[]{res.getColor(R.color.gradient_light), moodColor});
     }
 
+    /**
+     * Changes solid background color of given view.
+     *
+     * @param viewID
+     * @param moodColor
+     */
     private void setMoodBackground(int viewID, int moodColor) {
         findViewById(viewID).setBackgroundColor(moodColor);
     }
 
+    /**
+     * Sets text of given view.
+     *
+     * @param viewID
+     * @param text
+     */
     private void setMoodText(int viewID, String text) {
         ((TextView) findViewById(viewID)).setText(text);
     }
 
+    /**
+     * Performs all changes to implement a given mood, as indexed on WhatToDo.moodColors and string_array resource spinner_moods
+     *
+     * @param moodIndex
+     */
     private void setMood(int moodIndex) {
         // TODO rather than these hardcoded series, subclass the relevant views (and add custom fields where necessary)
 
@@ -209,7 +207,7 @@ public final class WhatToDo extends Activity {
         setMoodText(R.id.text_intro, moodStrings[0]);
         setMoodText(R.id.text_and, moodStrings[1]);
         setMoodText(R.id.button_did, moodStrings[2]);
-        // TODO get button to shrink to new text when text wraps. (apparently the white trailing space on the first line is counted as content)
+        // TODO get button to shrink to new text when text wraps. (Apparently the white trailing space on the first line is counted as content.)
         //findViewById(R.id.button_did).getLayoutParams().width = GridLayout.LayoutParams.WRAP_CONTENT;
 
         setMoodText(R.id.text_didnt, moodStrings[3]);
@@ -221,7 +219,12 @@ public final class WhatToDo extends Activity {
         setMoodText(R.id.button_bye, moodStrings[9]);
     }
 
-    public void onButtonClickDid(View view) throws IOException {
+    /**
+     * On button click, mark current task as done and select another from the current task list.
+     *
+     * @param view
+     */
+    public void onButtonClickDid(View view) {
         // TODO: rewrite in style of AsyncLoadTasks
         new AsyncTask<Void, Void, Task>() {
 
@@ -232,7 +235,6 @@ public final class WhatToDo extends Activity {
                         DateTime date = new DateTime(System.currentTimeMillis(), 0);
 
                         task.setStatus("completed");
-//                        task.setDue(date);
                         return service.tasks().update(tasklist.getId(), task.getId(), task).execute();
                     }
                 } catch (IOException e) {
@@ -250,19 +252,46 @@ public final class WhatToDo extends Activity {
         AsyncLoadTasks.run(this);
     }
 
+    /**
+     * On button click, choose a new current task.
+     *
+     * @param view
+     */
     public void onButtonClickElse(View view)
     {
         AsyncLoadTasks.run(this);
     }
 
+    /**
+     * On button click, "exit" the app.
+     * (It's not good practice in Android to actually exit; run Home activity instead.)
+     *
+     * @param view
+     */
     public void onButtonClickBye(View view)
     {
-        // Not good Android practice to actually exit; run Home activity instead
         Intent intent = new Intent(Intent.ACTION_MAIN);
         intent.addCategory(Intent.CATEGORY_HOME);
         intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
         startActivity(intent);
     }
+
+// re-used open source code from here
+/*
+ * Copyright (c) 2011 Google Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except
+ * in compliance with the License. You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software distributed under the License
+ * is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express
+ * or implied. See the License for the specific language governing permissions and limitations under
+ * the License.
+
+    MODIFIED for com.undrowned.WhatToDo 2013 Alex Davis
+ */
 
     void showGooglePlayServicesAvailabilityErrorDialog(final int connectionStatusCode) {
         runOnUiThread(new Runnable() {
@@ -374,5 +403,4 @@ public final class WhatToDo extends Activity {
     private void chooseAccount() {
         startActivityForResult(credential.newChooseAccountIntent(), REQUEST_ACCOUNT_PICKER);
     }
-
 }
